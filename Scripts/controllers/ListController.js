53426ca3810c55931e4b64ffcbd264dcb791b68e
DConstantinescu <denisa.constantinescu@teamnet.com>
2014-12-02 17:53:27 +0200

    Date persistente

--- Scripts/controllers/ListController.js
+++ Scripts/controllers/ListController.js
@@ -1,4 +1,4 @@
-shoppingApp.controller('ListController', ['$scope', function($scope) {
+shoppingApp.controller('ListController', ['$scope','$http','$location', function($scope,$http,$location) {
 
 //filtru de cautare
     $scope.startsWith = function (actual, expected) {
@@ -6,105 +6,129 @@ shoppingApp.controller('ListController', ['$scope', function($scope) {
         return lowerStr.indexOf(expected.toLowerCase()) === 0;
     }
 
-    var strikedProd= []; //lista produse bifate
-    var shoppingList = [
-    {
-        name: 'Sky',
-        quantity: 3,
-        check: 0,
-        edit: true
-    },
-    {
-        name: 'Cagula',
-        quantity: 3,
-        check: 0,
-        edit: true
-    },
-    {
-        name: 'Casca Sky',
-        quantity: 1,
-        check: 0,
-        edit: true
-    },
-    {
-        name: 'Clapari',
-        quantity: 2,
-        check: 0,
-        edit: true
-    },
-    {
-        name: 'Placa',
-        quantity: 1,
-        check: 0,
-        edit: true
-    },
-    {
-        name: 'Geaca',
-        quantity: 3,
-        check: 0,
-        edit: true
-    }
-    ,
-    {
-        name: 'Manusi',
-        quantity: 4,
-        check: 0,
-        edit: true
-    }
 
-];
-    $scope.shoppingList=shoppingList;
+    $http({url: 'http://localhost:8080/api/produse', method: 'GET'})
+        .success(function (data) {
+            $scope.shoppingList = data;
 
-    $scope.Prod={
+        });
+
+
+    $scope.adauga = function (Prod) {
+        $http({url: 'http://localhost:8080/api/produse', method: 'POST', data: Prod}).
+            success(function () {
+
+                $http({url: 'http://localhost:8080/api/produse', method: 'GET'})
+                    .success(function (data) {
+                        $scope.shoppingList = data;
+
+                    });
+
+
+            });
+    };
+
+
+    $scope.sterge = function () {
+        for (var i = 0; i < $scope.shoppingList.length; i++) {
+            if ($scope.shoppingList[i].check == "strikeout") {
+                console.log($scope.shoppingList[i]._id);
+                $http({url: 'http://localhost:8080/api/produse/' + $scope.shoppingList[i]._id, method: 'DELETE'})
+                    .success(function () {
+                        $http({url: 'http://localhost:8080/api/produse', method: 'GET'})
+                            .success(function (data) {
+                                $scope.shoppingList = data;
+                            });
+                    });
+            }
+        }
+    };
+
+
+    $scope.salveaza = function (prod, name, quantity) {//salveaza editare
+            console.log(name);
+        for (var i = 0; i < $scope.shoppingList.length; i++) {
+            if ($scope.shoppingList[i].name == prod.name) {
+                $http({url: 'http://localhost:8080/api/produse/'+$scope.shoppingList[i]._id, method: 'PUT', data:{name: name, quantity:quantity}})
+                    .success(function () {
+                        $http({url: 'http://localhost:8080/api/produse', method: 'GET'})
+                            .success(function (data) {
+                                $scope.shoppingList = data;
+                            });
+                    });
+            }
+        }
+    };
+
+
+    var strikedProd = []; //lista produse bifate
+//    var shoppingList = [
+//    {
+//        name: 'Sky',
+//        quantity: 3,
+//        check: 0,
+//        edit: true
+//    },
+//    {
+//        name: 'Cagula',
+//        quantity: 3,
+//        check: 0,
+//        edit: true
+//    },
+//    {
+//        name: 'Casca Sky',
+//        quantity: 1,
+//        check: 0,
+//        edit: true
+//    },
+//    {
+//        name: 'Clapari',
+//        quantity: 2,
+//        check: 0,
+//        edit: true
+//    },
+//    {
+//        name: 'Placa',
+//        quantity: 1,
+//        check: 0,
+//        edit: true
+//    },
+//    {
+//        name: 'Geaca',
+//        quantity: 3,
+//        check: 0,
+//        edit: true
+//    }
+//    ,
+//    {
+//        name: 'Manusi',
+//        quantity: 4,
+//        check: 0,
+//        edit: true
+//    }
+//
+//];
+    //  $scope.shoppingList=shoppingList;
+
+    $scope.Prod = {
         name: '',
         quantity: ''
     };
 
-    $scope.varShow=false;//pentru expandare +
-    $scope.show = function() {
-         $scope.varShow=!$scope.varShow;
+    $scope.varShow = false;//pentru expandare +
+    $scope.show = function () {
+        $scope.varShow = !$scope.varShow;
 
     };
 
 
-    $scope.showEdit = function(shop) {
+    $scope.showEdit = function (shop) {
         console.log(shop)
-        shop.edit=false;
+        shop.edit = false;
 
 
     };
 
-
-    $scope.adauga = function (prod) {//adaugare produs
-        shoppingList.push({
-            name: prod.name,
-            quantity: prod.quantity,
-            check: 0,
-            edit: true
-        });
-    };
-
-    $scope.salveaza = function (prod, name, quantity) {//salveaza editare
-
-        for(var i=0;i<shoppingList.length;i++)
-        {
-            if(shoppingList[i].name==prod.name)
-            {   shoppingList[i].edit=true;
-                shoppingList[i].name=name;
-                shoppingList[i].quantity=quantity;
-            }
-        }
-};
-
-    $scope.sterge= function () {//stergere produse bifate
-        for(i=0; i<shoppingList.length;i++){
-            for (j=0; j<strikedProd.length;j++){
-                if(shoppingList[i].name==strikedProd[j].name) shoppingList.splice(i,1);
-            }
-
-        }
-
-    };
 
     $scope.checkProd = function (Prod) {
         if (Prod.check == 0) {
@@ -115,11 +139,11 @@ shoppingApp.controller('ListController', ['$scope', function($scope) {
             Prod.check = 0, strikedProd.pop(Prod);
         }
         console.log(strikedProd);
-    }
-
-
+    };
 
 }]);
+
+
 
 shoppingApp.directive("ListaInregistrari", [
     function(){
